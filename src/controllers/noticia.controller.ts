d9b03e98c460ec459b20668d90ce0b390dad66fa
AngelGugliuzzo <angelgugliuzzo@gmail.com>
2023-12-06 16:41:35 -0300

    Initial commit

---
+++ src/controllers/noticia.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import logger from '../helpers/logger';
+import { Inoticias_create } from '../interfaces/noticias/noticias.interfaces';
+import { IsNull } from 'typeorm';
+
+export const noticiasIndex = (req: Request, res: Response) => {
+	// Aca devolver todas las noticias ordenadas por fecha
+	// ver documentacion de typeorm
+	const nombre = 'Pedro';
+	res.render('home/index', { nombre });
+};
+
+export const crearNoticiaView = (req: Request, res: Response) => {
+	res.render('noticias/crear');
+};
+
+export const crearNoticia = (req: Request, res: Response) => {
+	const data: Inoticias_create = req.body;
+
+	if (data.titulo_noticia.trim()=== '' || data.desc_noticia.trim()==='') {
+		res.render('shared/error');
+	}
+//---------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+	//todo: aca guardo en la base de datos
+	
+	console.log(data);
+
+	res.redirect('/noticias');
+};
